7acc792ef7be7406efda9dc998c13d36ce04843c
Zeenat-Mohammad <135528133+Zeenat-Mohammad@users.noreply.github.com>
2025-09-26 07:40:09 +0530

    Add files via upload

---
+++ print_bakward.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"Enter a number: ";
+    cin>>num;
+    while(num!=0){
+        cout<<num<<" ";
+        num--;
+    }
+    return 0;
+}
+
