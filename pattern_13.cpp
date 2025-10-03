9ea4c27eefba967c6d83593af795aac8064c1cd7
Zeenat-Mohammad <135528133+Zeenat-Mohammad@users.noreply.github.com>
2025-10-03 20:17:45 +0530

    Add files via upload

---
+++ pattern_13.cpp
@@ -0,0 +1,24 @@
+/* pattern
+        * 
+       * * 
+      * * * 
+     * * * * 
+*/
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=n-1;j>=i;j--){
+            cout<<" ";
+        }
+        for(int j=0;j<=i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
